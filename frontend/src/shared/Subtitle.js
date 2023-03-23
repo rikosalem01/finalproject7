1812f1c782bcfe13eb112d51a5a21b2f1146632e
fachri34 <115094648+fachri34@users.noreply.github.com>
2023-03-23 13:45:56 +0700

    edit css

--- frontend/src/shared/Subtitle.js
+++ frontend/src/shared/Subtitle.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Subtitle = ({subtitle}) => {
   return (
     <div>
-      <h3 className='section__subtitle'>{subtitle}</h3>
+      <h3 className='section-subtitle'>{subtitle}</h3>
     </div>
   )
 }
