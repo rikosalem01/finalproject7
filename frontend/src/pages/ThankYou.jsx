828ec4003105c9d2a8f09a4bd221742d1345423e
fachri34 <115094648+fachri34@users.noreply.github.com>
2023-03-23 12:54:57 +0700

    coba pull

--- frontend/src/pages/ThankYou.jsx
+++ frontend/src/pages/ThankYou.jsx
@@ -9,11 +9,11 @@ const ThankYou = () => {
             <Container>
                 <Row>
                     <Col lg='12' className='pt-5 text-center'>
-                        <div className='thank__you'>
+                        <div className='thank-you'>
                             <span><i className='ri-checkbox-circle-line'></i></span>
                             <h1 className='mb-3 fw-semibold'>Thank You</h1>
                             <h3 className='mb-4'>your tour is booked</h3>
-                            <Button className='btn primary__btn w-25'><Link to='/home'>Back to Home</Link></Button>
+                            <Button className='btn primary-btn w-25'><Link to='/home'>Back to Home</Link></Button>
                         </div>
                     </Col>
                 </Row>
